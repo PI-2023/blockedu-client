9d6290eedebbb240b2f6a8f320da70a08b8825d5
Isaque Lemos <isaquelemos_bds@hotmail.com>
2023-05-25 19:57:33 -0300

    Feat: adicionando classes e testes de conexao com o banco

---
+++ src/dao/ConexaoBancoDAOException.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class ConexaoBancoDAOException extends Exception {
+  public ConexaoBancoDAOException() {
+    super();
+  }
+
+  public ConexaoBancoDAOException(String mensagem) {
+    super(mensagem);
+  }
+}
---
+++ src/dao/ConexaoBancoDAOTest.java
@@ -0,0 +1,32 @@
+package dao;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+
+
+public class ConexaoBancoDAOTest {
+
+  private static final String BANCO = "null";
+  private static final String SENHA = "null";
+  private static final String USUARIO = "null";
+
+  @Test
+  public void deveEstabelecerConexao() throws ConexaoBancoDAOException {
+    try {
+      Connection conexao = ConexaoBancoDAO.getConexao();
+      assertNotNull(conexao);
+    } catch (ConexaoBancoDAOException e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void naoDeveEstabelecerConexao() throws ConexaoBancoDAOException {
+    assertThrows(ConexaoBancoDAOException.class, () -> ConexaoBancoDAO.getConexao(BANCO, USUARIO, SENHA));
+  }
+}
