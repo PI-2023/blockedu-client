741c02a65a2c6d70d8fe60c13703215b1dc81cad
Isaque Lemos <isaquelemos_bds@hotmail.com>
2023-05-26 20:17:34 -0300

    Fix: atualizando id AlunoVO

--- src/vo/AlunoVO.java
+++ src/vo/AlunoVO.java
@@ -3,18 +3,28 @@ package vo;
 import java.time.LocalDate;
 
 public class AlunoVO {
+  private int id;
   private String cpf;
   private String nome;
   private String email;
   private String telefoneCelular;
   private LocalDate dataNascimento;
 
-  public AlunoVO(String cpf, String nome, String email, LocalDate dataNascimento, String telefoneCelular) {
+  public AlunoVO(int id, String cpf, String nome, String email, LocalDate dataNascimento, String telefoneCelular) {
+    this.id = id;
     this.cpf = cpf;
     this.nome = nome;
     this.email = email;
     this.dataNascimento = dataNascimento;
     this.telefoneCelular = telefoneCelular;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getCpf() {
