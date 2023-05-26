e22fe6fd36b4057d8b9bd471cf767c887111b1b6
Isaque Lemos <isaquelemos_bds@hotmail.com>
2023-05-26 20:17:57 -0300

    Feat: implementando métodos de AlunoDAO

--- src/dao/AlunoDAO.java
+++ src/dao/AlunoDAO.java
@@ -1,6 +1,13 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import vo.AlunoVO;
@@ -25,45 +32,124 @@ public class AlunoDAO implements IAlunoDAO {
 	}
 
   @Override
-  public AlunoVO buscar(String cpf) {
-    // Não implementado
-    throw new UnsupportedOperationException("Método 'buscar' não implementado");
+  public AlunoVO buscar(String cpf) throws AlunoDAOException {
+    try {
+      PreparedStatement stmt = this.conexao.prepareStatement("SELECT * FROM Aluno WHERE cpf = ?;");
+      
+      stmt.setString(1, cpf);
+      ResultSet rset = stmt.executeQuery();
+
+      if (rset.next()) {
+        return criarAlunoVO(rset);
+      }
+    } catch (Exception e) {
+      throw new AlunoDAOException(e.getMessage());
+    }
+
+    return null;
   }
 
   @Override
-  public AlunoVO buscar(Integer id) {
-    // Não implementado
-    throw new UnsupportedOperationException("Método 'buscar' não implementado");
+  public AlunoVO buscar(Integer id) throws AlunoDAOException {
+    try {
+      PreparedStatement stmt = this.conexao.prepareStatement("SELECT * FROM Aluno WHERE id = ?;");
+      
+      stmt.setInt(1, id);
+      ResultSet rset = stmt.executeQuery();
+
+      if (rset.next()) {
+        return criarAlunoVO(rset);
+      }
+    } catch (Exception e) {
+      throw new AlunoDAOException(e.getMessage());
+    }
+
+    return null;
   }
 
   @Override
-  public List<AlunoVO> buscarTodos() {
-    // Não implementado
-    throw new UnsupportedOperationException("Método 'buscarTodos' não implementado");
+  public List<AlunoVO> buscarTodos() throws AlunoDAOException {
+    List<AlunoVO> alunos = new ArrayList<>();
+
+    try {
+      Statement stmt = this.conexao.createStatement();
+      ResultSet rset = stmt.executeQuery("SELECT * FROM Aluno;");
+
+      while (rset.next()) {
+        AlunoVO aluno = criarAlunoVO(rset);
+        alunos.add(aluno);
+      }
+    } catch (Exception e) {
+      throw new AlunoDAOException(e.getMessage());
+    }
+
+    return alunos;
   }
 
   @Override
-  public void inserir(AlunoVO alunoVO) {
-    // Não implementado
-    throw new UnsupportedOperationException("Método 'inserir' não implementado");
+  public void inserir(AlunoVO alunoVO) throws AlunoDAOException {
+    String query = "INSERT INTO Aluno (cpf, nome, email, data_nascimento, telefone_celular) VALUES (?, ?, ?, ?, ?);";
+
+    try {
+      PreparedStatement stmt = this.conexao.prepareStatement(query);
+      stmt.setString(1, alunoVO.getCpf());
+      stmt.setString(2, alunoVO.getNome());
+      stmt.setString(3, alunoVO.getEmail());
+      stmt.setDate(4, Date.valueOf(alunoVO.getDataNascimento()));
+      stmt.setString(5, alunoVO.getTelefoneCelular());
+      stmt.executeUpdate();
+    } catch (Exception e) {
+      throw new AlunoDAOException(e.getMessage());
+    }
   }
 
   @Override
-  public void atualizar(AlunoVO alunoVO) {
-    // Não implementado
-    throw new UnsupportedOperationException("Método 'atualizar' não implementado");
+  public void atualizar(AlunoVO alunoVO) throws AlunoDAOException {
+    String query = "UPDATE Aluno SET cpf = ?, nome = ?, email = ?, data_nascimento = ?, telefone_celular = ? WHERE id = ?;";
+
+    try {
+      PreparedStatement stmt = this.conexao.prepareStatement(query);
+      stmt.setString(1, alunoVO.getCpf());
+      stmt.setString(2, alunoVO.getNome());
+      stmt.setString(3, alunoVO.getEmail());
+      stmt.setDate(4, Date.valueOf(alunoVO.getDataNascimento()));
+      stmt.setString(5, alunoVO.getTelefoneCelular());
+      stmt.setInt(6, alunoVO.getId());
+      stmt.executeUpdate();
+    } catch (Exception e) {
+      throw new AlunoDAOException(e.getMessage());
+    }
   }
 
   @Override
-  public void excluir(String cpf) {
-    // Não implementado
-    throw new UnsupportedOperationException("Método 'excluir' não implementado");
+  public void excluir(String cpf) throws AlunoDAOException {
+    try {
+      PreparedStatement stmt = this.conexao.prepareStatement("DELETE FROM Aluno WHERE cpf = ?;"); 
+      stmt.setString(1, cpf);
+      stmt.executeUpdate();
+    } catch (Exception e) {
+      throw new AlunoDAOException(e.getMessage());
+    }
   }
 
   @Override
-  public void excluir(Integer id) {
-    // Não implementado
-    throw new UnsupportedOperationException("Método 'excluir' não implementado");
+  public void excluir(Integer id) throws AlunoDAOException {
+    try {
+      PreparedStatement stmt = this.conexao.prepareStatement("DELETE FROM Aluno WHERE id = ?;"); 
+      stmt.setInt(1, id);
+      stmt.executeUpdate();
+    } catch (Exception e) {
+      throw new AlunoDAOException(e.getMessage());
+    }  
   }
 
+  private AlunoVO criarAlunoVO(ResultSet rset) throws SQLException {
+    int id = rset.getInt("id");
+    String cpf = rset.getString("cpf");
+    String nome = rset.getString("nome");
+    String email = rset.getString("email");
+    String telefoneCelular = rset.getString("telefone_celular");
+    LocalDate dataNascimento = rset.getDate("data_nascimento").toLocalDate();
+    return new AlunoVO(id, cpf, nome, email, dataNascimento, telefoneCelular);
+  }
 }
--- src/dao/IAlunoDAO.java
+++ src/dao/IAlunoDAO.java
@@ -10,42 +10,42 @@ public interface IAlunoDAO {
    * @param cpf Documento de identificação único do aluno;
    * @return Instância de AlunoVO.
    */
-  AlunoVO buscar(String cpf);
+  AlunoVO buscar(String cpf) throws AlunoDAOException;
 
   /**
    * Busca um aluno na base de dados por id.
    * @param id Identificador único do aluno gerado pelo banco;
    * @return Instância de AlunoVO.
    */
-  AlunoVO buscar(Integer id);
+  AlunoVO buscar(Integer id) throws AlunoDAOException;
   
   /**
    * Busca todos os alunos cadastrados na base de dados.
    * @return Uma lista de instâncias de AlunoVO.
    */
-  List<AlunoVO> buscarTodos();
+  List<AlunoVO> buscarTodos() throws AlunoDAOException;
   
   /**
    * Insere um novo aluno na base de dados.
    * @param alunoVO Instância de AlunoVO;
    */
-  void inserir(AlunoVO alunoVO);
+  void inserir(AlunoVO alunoVO) throws AlunoDAOException;
   
   /**
    * Atualiza um aluno na base de dados.
    * @param alunoVO Instância de AlunoVO;
    */
-  void atualizar(AlunoVO alunoVO);
+  void atualizar(AlunoVO alunoVO) throws AlunoDAOException;
 
   /**
    * Exclui um aluno na base de dados por cpf.
    * @param cpf Documento de identificação único do aluno;
    */
-  void excluir(String cpf);
+  void excluir(String cpf) throws AlunoDAOException;
 
   /**
    * Exclui um aluno na base de dados por id.
    * @param id Documento de identificação único do aluno;
    */
-  void excluir(Integer id);
+  void excluir(Integer id) throws AlunoDAOException;
 }
