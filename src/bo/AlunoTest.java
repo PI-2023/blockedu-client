bb555a3123b6f57082bf2fa54d456040c87763cf
andreoluiz <andrecardosoal99@gmail.com>
2023-05-27 12:28:39 -0300

    Test Junit: Bo Aluno e Vo Aluno

--- src/bo/AlunoTest.java
+++ src/bo/AlunoTest.java
@@ -1,48 +1,34 @@
 package bo;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
+import java.time.Month;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AlunoTest {
 
     @Test
-    public void testSetCpf_InvalidCpf_ThrowsException() {
-        String cpf = "1234567890"; // Invalido - 10 digitos
-        Aluno aluno = new Aluno("123456789", "João", "joao@teste.com", LocalDate.of(1997, 08, 16), "12345678901");
+    public void testSetCpf(){
+        Aluno aluno = new Aluno("12345678901", "Andrézin rei delas", "andrecardosoal99@gmail.com", LocalDate.of(1997, Month.AUGUST, 16), "82994227272");
+        aluno.setCpf("12345678901");
+        assertEquals("12345678901", aluno.getCpf());
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            aluno.setCpf(cpf);
-        });
+        try {aluno.setCpf("123456780");
+            fail("Deve lançar uma exception");
+        } catch (IllegalArgumentException e) {
+            assertEquals("CPF inválido", e.getMessage());
+        }
     }
-
     @Test
-    public void testSetCpf_ValidCpf_SetsCpf() {
-        String cpf = "12345678901"; // CPF válido - 11 digitos
-        Aluno aluno = new Aluno("12345678901", "João", "joao@teste.com", LocalDate.of(1955, 10, 23), "12345678901");
+    public void testSetEmail(){
+        Aluno aluno = new Aluno("12345678901", "Andrézin rei delas", "andrecardosoal99@gmail.com", LocalDate.of(1997, Month.AUGUST, 16), "82994227272");
+        assertEquals("andrecardosoal99@gmail.com", aluno.getEmail());
 
-        aluno.setCpf(cpf);
-
-        Assertions.assertEquals(cpf, aluno.getCpf());
-    }
-
-    @Test
-    public void testSetTelefoneCelular_InvalidTelefone_ThrowsException() {
-        String telefone = "1234567890"; // Telefone inválido - 10 digitos
-        Aluno aluno = new Aluno("12345678901", "João", "joao@teste.com", LocalDate.of(1895, 05, 15), "12345678901");
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            aluno.setTelefoneCelular(telefone);
-        });
-    }
-
-    @Test
-    public void testSetTelefoneCelular_ValidTelefone_SetsTelefone() {
-        String telefone = "12345678901"; // Telefone válido - 11 digitos
-        Aluno aluno = new Aluno("12345678901", "João", "joao@teste.com", LocalDate.of(2022, 10, 15), "00000000000");
-
-        aluno.setTelefoneCelular(telefone);
-
-        Assertions.assertEquals(telefone, aluno.getTelefoneCelular());
-
+        try {
+            aluno.setEmail("");
+            fail("Deve lançar uma exception");
+        } catch (IllegalArgumentException e) {
+            assertEquals("O campo email está vazio", e.getMessage());
+        }
     }
 }
