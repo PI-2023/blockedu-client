a18ef9ada48c7e0a89ca5bf1ed9528f2608fea03
DenixVieira <mcdenixson@gmail.com>
2023-06-04 18:54:02 -0300

    Corrigindo o Socket cliente e apagando a pasta de 'SocketseViewsSockets'

--- src/so/SocketClient.java
+++ src/so/SocketClient.java
@@ -1,8 +1,8 @@
 package so;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -16,17 +16,19 @@ public class SocketClient {
 
     public String enviarCertificado(String certificado) {
         try (Socket socket = new Socket(HOST, PORT)) {
-            OutputStream outputStream = socket.getOutputStream();
-            InputStream inputStream = socket.getInputStream();
 
-            outputStream.write(certificado.getBytes());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+
+            outputStream.writeUTF(certificado);
             outputStream.flush();
+            
+            String hash = inputStream.readUTF();
 
-            byte[] buffer = new byte[1024];
-            int bytesRead = inputStream.read(buffer);
-            String hash = new String(buffer, 0, bytesRead);
+            socket.close();
 
             return hash;
+
         } catch (IOException e) {
             e.printStackTrace();
             return null;
